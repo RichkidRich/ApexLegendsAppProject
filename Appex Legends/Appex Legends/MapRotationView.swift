29f745df7b80f49049019384c9384cd8b6ac8569
RichkidRich <richardlee076@gmail.com>
2021-07-02 16:20:40 -0400

    AP-003 Add mpa rotation view file

---
+++ Appex Legends/Appex Legends/MapRotationView.swift
@@ -0,0 +1,22 @@
+//
+//  MapRotationView.swift
+//  Appex Legends
+//
+//  Created by Richard Lee on 2021-07-02.
+//
+
+import SwiftUI
+import CoreData
+
+struct MapRotationView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct MapRotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapRotationView()
+    }
+}
